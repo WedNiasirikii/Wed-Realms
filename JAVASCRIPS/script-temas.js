0629ad79d75ad061bbb341e529982ab9a1106c71
WedNiasirikii <cm7383173@gmail.com>
2024-07-05 19:02:55 -0300

    Responsiv

--- JAVASCRIPS/script-temas.js
+++ JAVASCRIPS/script-temas.js
@@ -39,31 +39,9 @@ function temaBamboo() {
       "url(./IMAGENES/temas/bamboo/Block_of_Bamboo_29_JE1.webp)";
   });
 
-  document.querySelectorAll("h2").forEach((BambooH2) => {
-    BambooH2.style.color = "yellow";
-  });
+  document.documentElement.style.setProperty("--color-texto-simple", "white");
 
-  document.querySelectorAll(".recurse-title").forEach((BambooRecurseTitle) => {
-    BambooRecurseTitle.style.color = "Green";
-  });
-
-  document.querySelectorAll(".DownloadButton").forEach((BambooDownload) => {
-    BambooDownload.style.backgroundColor = "Green";
-  });
-
-  document.querySelectorAll(".seeds button").forEach((BambooCopyBoton) => {
-    BambooCopyBoton.style.backgroundImage =
-      "url(./IMAGENES/temas/bamboo/Bamboo_Mosaic_29_JE1_BE1.webp)";
-
-    BambooCopyBoton.style.color = "White";
-  });
-
-  document
-    .querySelectorAll(".DownloadButton")
-    .forEach((BambooDownloadBoton) => {
-      BambooDownloadBoton.style.backgroundImage =
-        "url(./IMAGENES/temas/bamboo/Block_of_Bamboo_29_JE1.webp)";
-    });
+  document.documentElement.style.setProperty("--color-texto-fuerte", "yellow");
 }
 
 //cambio a temaRocaNegra
@@ -105,33 +83,9 @@ function temaRocaNegra() {
       "url(./IMAGENES/temas/roca-negra/Smooth_Basalt_29_JE1.webp)";
   });
 
-  document.querySelectorAll("h2").forEach((RocaNegraH2) => {
-    RocaNegraH2.style.color = "orange";
-  });
+  document.documentElement.style.setProperty("--color-texto-simple", "white");
 
-  document
-    .querySelectorAll(".recurse-title")
-    .forEach((RocaNegraRecurseTitle) => {
-      RocaNegraRecurseTitle.style.color = "orange";
-    });
-
-  document.querySelectorAll(".DownloadButton").forEach((RocaNegraDownload) => {
-    RocaNegraDownload.style.backgroundImage =
-      "url(./IMAGENES/temas/roca-negra/Smooth_Basalt_29_JE1.webp)";
-  });
-
-  document
-    .querySelectorAll(".recurce-text-explicacion")
-    .forEach((RocaNegraText) => {
-      RocaNegraText.style.color = "white";
-    });
-
-  document.querySelectorAll(".seeds button").forEach((RocaNegraCopyBoton) => {
-    RocaNegraCopyBoton.style.backgroundImage =
-      "url(./IMAGENES/temas/roca-negra/fondo-piedra-negra-body.jpg.jpg)";
-
-    RocaNegraCopyBoton.style.color = "white";
-  });
+  document.documentElement.style.setProperty("--color-texto-fuerte", "orange");
 }
 
 //cambio a tema End
@@ -157,10 +111,6 @@ function temaEnd() {
       "url(./IMAGENES/temas/end/End_Stone_29_JE3_BE2.webp)";
   });
 
-  document.querySelectorAll(".links a").forEach((EndLinkText) => {
-    EndLinkText.style.color = "purple";
-  });
-
   document.querySelectorAll(".resource").forEach((EndRecurse) => {
     EndRecurse.style.backgroundImage =
       "url(./IMAGENES/temas/end/End_Stone_Bricks_29_JE2_BE2.webp)";
@@ -171,23 +121,7 @@ function temaEnd() {
       "url(./IMAGENES/temas/end/Purpur_Block_29_JE2_BE2.webp)";
   });
 
-  document.querySelectorAll(".recurce-text-explicacion").forEach((EndText) => {
-    EndText.style.color = "black";
-  });
+  document.documentElement.style.setProperty("--color-texto-simple", "black");
 
-  document.querySelectorAll(".recurse-title").forEach((EndRecurseTitle) => {
-    EndRecurseTitle.style.color = "purple";
-  });
-
-  document.querySelectorAll(".DownloadButton").forEach((EndDownloadBoton) => {
-    EndDownloadBoton.style.backgroundImage =
-      "url(./IMAGENES/temas/end/Purpur_Block_29_JE2_BE2.webp)";
-  });
-
-  document.querySelectorAll(".seeds button").forEach((EndCopyBoton) => {
-    EndCopyBoton.style.backgroundImage =
-      "url(./IMAGENES/temas/end/End_Stone_Bricks_29_JE2_BE2.webp)";
-
-    EndCopyBoton.style.color = "purple";
-  });
+  document.documentElement.style.setProperty("--color-texto-fuerte", "purple");
 }
