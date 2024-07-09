66440fbacd65539dde2f5de4685c0bbbbb6d82c4
WedNiasirikii <cm7383173@gmail.com>
2024-07-08 22:23:37 -0300

    galeria

--- JAVASCRIPS/script-temas.js
+++ JAVASCRIPS/script-temas.js
@@ -3,36 +3,15 @@ function temaBamboo() {
   const ClickSound = new Audio("./SONIDOS/29960.mp3");
   ClickSound.play();
 
-  // Cambia el fondo del cuerpo
-  document.getElementById("contenedorBody").style.backgroundImage =
-    "url(./IMAGENES/temas/bamboo/Block_of_Stripped_Bamboo_29_JE1_BE1.webp)";
+  document.documentElement.style.setProperty(
+    "--fondo-fuerte",
+    "url(../IMAGENES/temas/bamboo/Block_of_Stripped_Bamboo_29_JE1_BE1.webp)"
+  );
 
-  // Cambia el fondo de todas las secciones
-  document.querySelectorAll("section").forEach((section) => {
-    section.style.backgroundImage =
-      "url(./IMAGENES/temas/bamboo/Block_of_Bamboo_29_JE1.webp)";
-  });
-
-  document.querySelectorAll(".icons-temas").forEach((mosaicos) => {
-    mosaicos.style.backgroundImage =
-      "url(./IMAGENES/temas/bamboo/Bamboo_Mosaic_29_JE1_BE1.webp)";
-  });
-
-  document.querySelectorAll(".links-containers").forEach((mosaicosLink) => {
-    mosaicosLink.style.backgroundImage =
-      "url(./IMAGENES/temas/bamboo/Bamboo_Mosaic_29_JE1_BE1.webp)";
-
-    mosaicosLink.style.color = "white";
-  });
-  document.querySelectorAll(".resource").forEach((mosaicosRecurse) => {
-    mosaicosRecurse.style.backgroundImage =
-      "url(./IMAGENES/temas/bamboo/Bamboo_Mosaic_29_JE1_BE1.webp)";
-  });
-
-  document.querySelectorAll("footer").forEach((mosaicosFooter) => {
-    mosaicosFooter.style.backgroundImage =
-      "url(./IMAGENES/temas/bamboo/Block_of_Bamboo_29_JE1.webp)";
-  });
+  document.documentElement.style.setProperty(
+    "--fondo-simple",
+    "url(../IMAGENES/temas/bamboo/Block_of_Bamboo_29_JE1.webp)"
+  );
 
   document.documentElement.style.setProperty("--color-texto-simple", "white");
 
@@ -49,34 +28,16 @@ function temaRocaNegra() {
   //Libera un sonido de "click"
   const ClickSound = new Audio("./SONIDOS/29960.mp3");
   ClickSound.play();
-  // Cambia el fondo del cuerpo
-  document.getElementById("contenedorBody").style.backgroundImage =
-    "url(./IMAGENES/temas/roca-negra/fondo-piedra-negra-body.jpg.jpg)";
 
-  // Cambia el fondo de todas las secciones
-  document.querySelectorAll("section").forEach((section) => {
-    section.style.backgroundImage =
-      "url(./IMAGENES/temas/roca-negra/Smooth_Basalt_29_JE1.webp)";
-  });
+  document.documentElement.style.setProperty(
+    "--fondo-fuerte",
+    "url(../IMAGENES/temas/roca-negra/fondo-piedra-negra-body.jpg.jpg)"
+  );
 
-  document.querySelectorAll(".icons-temas").forEach((RocaNegra) => {
-    RocaNegra.style.backgroundImage =
-      "url(./IMAGENES/temas/roca-negra/fondo-piedra-negra-body.jpg.jpg)";
-  });
-
-  document.querySelectorAll(".links-containers").forEach((RocaNegraLink) => {
-    RocaNegraLink.style.backgroundImage =
-      "url(./IMAGENES/temas/roca-negra/fondo-piedra-negra-body.jpg.jpg)";
-  });
-  document.querySelectorAll(".resource").forEach((RocaNegraRecurse) => {
-    RocaNegraRecurse.style.backgroundImage =
-      "url(./IMAGENES/temas/roca-negra/fondo-piedra-negra-body.jpg.jpg)";
-  });
-
-  document.querySelectorAll("footer").forEach((RocaNegraFooter) => {
-    RocaNegraFooter.style.backgroundImage =
-      "url(./IMAGENES/temas/roca-negra/Smooth_Basalt_29_JE1.webp)";
-  });
+  document.documentElement.style.setProperty(
+    "--fondo-simple",
+    "url(../IMAGENES/temas/roca-negra/Smooth_Basalt_29_JE1.webp)"
+  );
 
   document.documentElement.style.setProperty("--color-texto-simple", "white");
 
